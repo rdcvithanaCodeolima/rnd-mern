30fc5a293b95bb4c51c0cfc027191b3bbbe7c13e
rdcvithana <rdcvithana@gmail.com>
2024-03-22 15:26:15 +0530

    update footer and header

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
 import Header from './Components/Header';
 import Content from './Components/Content';
 import Footer from './Components/Footer';
 
 function App() {
-  return (
-      <div>
-        <Header /> 
-        <Content />
-        <Footer />
 
-      </div>
+  const appName = 'Codeolima'
+  const Version = '1.0.0'
+
+  return (
+      <>
+        <Header name={appName}/> 
+        <Content />
+        <Footer version={Version}/>
+
+      </>
   );
 }
 
--- client/src/Components/Footer.js
+++ client/src/Components/Footer.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 export default class Header extends Component {
   render() {
     return (
-      <div>footer</div>
+      <div>Version : {this.props.version}</div>
     )
   }
 }
