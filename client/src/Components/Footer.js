78c9bd8c9533820aed3e6deb7a4423ef5f601903
rdcvithanaCodeolima <162977713+rdcvithanaCodeolima@users.noreply.github.com>
2024-03-22 15:28:25 +0530

    Delete client/src/Components directory

--- client/src/Components/Footer.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react'
-
-export default class Header extends Component {
-  render() {
-    return (
-      <div>Version : {this.props.version}</div>
-    )
-  }
-}
